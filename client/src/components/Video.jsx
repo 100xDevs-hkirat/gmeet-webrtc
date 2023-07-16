75d099efa17ab83f25cafca8219a2e9fbf0b12f5
Amit Ranjan <amitalable@gmail.com>
2023-07-16 21:59:45 +0530

    Added change

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,18 +1,26 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useRef } from "react";
 
-export const Video = ({stream}) => {
-    const videoRef = useRef();
-    useEffect(() => {
-        if (videoRef && videoRef.current) {
-            videoRef.current.srcObject = stream;
-        }
-      }, [videoRef])
-    
-      return (
-        <div>
-          <div>
-            <video style={{borderRadius: 10}} ref={videoRef} muted width="100%" autoPlay={true} playsInline={true} />
-          </div>
-        </div>
-      )
+// eslint-disable-next-line react/prop-types
+export const Video = ({ stream, tracks }) => {
+  const videoRef = useRef();
+  useEffect(() => {
+    if (videoRef && videoRef.current) {
+      if (stream) {
+        videoRef.current.srcObject = stream;
+      } else if (tracks.length > 0) {
+        // Create a new MediaStream with all remote tracks
+        const remoteStream = new MediaStream(tracks);
+        videoRef.current.srcObject = remoteStream;
+      }
+    }
+  }, [videoRef, tracks])
+
+  return (
+    <div>
+      <div>
+        <video style={{ borderRadius: 10 }} ref={videoRef} muted width="100%" autoPlay={true} playsInline={true} />
+      </div>
+    </div>
+  )
 }
