b7d0eb268e4e37408ca7cac1082fd7ce38e29739
Satya Nishanth <satya@Satyas-MacBook-Pro.local>
2023-07-16 21:28:47 +0530

    fixed with force render

--- client/src/components/MeetingPage.jsx
+++ client/src/components/MeetingPage.jsx
@@ -5,57 +5,60 @@ import { Button, Grid, Typography } from "@mui/material"
 import { CentralizedCard } from "./CentralizedCard";
 import { Video } from "./Video";
 
+ let pc = new RTCPeerConnection({
+   iceServers: [
+     {
+       urls: "stun:stun.l.google.com:19302",
+     },
+   ],
+ });
 export function MeetingPage() {
     const [socket, setSocket] = useState(null);
     const [meetingJoined, setMeetingJoined] = useState(false);
     const [videoStream, setVideoStream] = useState();
     const [remoteVideoStream, setRemoteVideoStream] = useState();
+    const [_,forceRender] = useState(null)
     
     const params = useParams();
     const roomId = params.roomId;
 
     useEffect(() => {
-        const s = socketIO.connect('http://localhost:3000');
-        s.on("connect", () => {
-            setSocket(s);
-            s.emit("join", {
-                roomId
-            })
-
-            window.navigator.mediaDevices.getUserMedia({
-                video: true
-            }).then(async (stream) => {
-                setVideoStream(stream);
-            });
-    
-            s.on("localDescription", async ({description}) => {
-                // Receiving video - 
-                let pc = new RTCPeerConnection({
-                    iceServers: [
-                    {
-                        urls: "stun:stun.l.google.com:19302",
-                    },
-                    ],
-                });
-                
-                pc.setRemoteDescription(description);
-                pc.ontrack = (e) => {
-                    setRemoteVideoStream(new MediaStream([e.track]))
-                }
-
-                s.on("iceCandidate", ({candidate}) => {
-                    pc.addIceCandidate(candidate)
-                });
-
-                pc.onicecandidate = ({candidate}) => {
-                    s.emit("iceCandidateReply", {candidate});
-                }
-
-                await pc.setLocalDescription(await pc.createAnswer());
-                s.emit("remoteDescription", {description: pc.localDescription});
-            })
+      const s = socketIO.connect("http://localhost:3001");
+      s.on("connect", () => {
+        setSocket(s);
+        s.emit("join", {
+          roomId,
         });
-    }, [])
+
+        window.navigator.mediaDevices
+          .getUserMedia({
+            video: true,
+          })
+          .then(async (stream) => {
+            setVideoStream(stream);
+          });
+
+        s.on("localDescription", async ({ description }) => {
+          // Receiving video -
+          pc.setRemoteDescription(description);
+          pc.ontrack = (e) => {
+              setRemoteVideoStream(new MediaStream([e.track]));
+              forceRender()
+          };
+
+          s.on("iceCandidate", ({ candidate }) => {
+            pc.addIceCandidate(candidate);
+          });
+
+          pc.onicecandidate = ({ candidate }) => {
+            s.emit("iceCandidateReply", { candidate });
+          };
+
+          await pc.setLocalDescription(await pc.createAnswer());
+          s.emit("remoteDescription", { description: pc.localDescription });
+        });
+      });
+    }, []);
 
     if (!videoStream) {
         return <div>
@@ -75,13 +78,6 @@ export function MeetingPage() {
                 <div style={{display: "flex", justifyContent: "center"}}>
                     <Button onClick={() => {
                         // sending pc
-                        let pc = new RTCPeerConnection({
-                            iceServers: [
-                              {
-                                urls: "stun:stun.l.google.com:19302",
-                              },
-                            ],
-                          });
                         pc.onicecandidate = ({candidate}) => {
                             socket.emit("iceCandidate", {candidate});
                         }
@@ -110,13 +106,13 @@ export function MeetingPage() {
             </CentralizedCard>
         </div>
     }
-
+    console.log({remoteVideoStream})
     return <Grid container spacing={2} alignContent={"center"} justifyContent={"center"}>
         <Grid item xs={12} md={6} lg={4}>
             <Video stream={videoStream} />
         </Grid>
         <Grid item xs={12} md={6} lg={4}>
-            <Video stream={remoteVideoStream} />
+            {remoteVideoStream && <Video stream={remoteVideoStream} />}
         </Grid>
     </Grid>
 }
